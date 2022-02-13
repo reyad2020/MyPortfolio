582fdd189538fb86e950b7638244bf6ce535a4dd
reyad <reyad@DESKTOP-NGQ9GTD>
2022-02-13 21:09:58 +0200

    Add project files.

---
+++ Core/Entities/Owner.cs
@@ -0,0 +1,10 @@
+﻿namespace Core.Entities
+{
+  public  class Owner : EntityBase
+    {
+        public string FullName { get; set; }
+        public string Avatar { get; set; }
+        public string Profile { get; set; }
+        Address Address { get; set; }
+    }
+}
